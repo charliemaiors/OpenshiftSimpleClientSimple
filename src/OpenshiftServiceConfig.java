7503f063fabbdb442a1c311d6421cb4c7a6b9cc5
Pasquale Carlo Maiorano Picone <charliemaiors@gmail.com>
2016-03-05 10:48:47 +0100

    Update OpenshiftServiceConfig.java

--- src/OpenshiftServiceConfig.java
+++ src/OpenshiftServiceConfig.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 
 public class OpenshiftServiceConfig {
 
-    private final static String consoleIpAddress = "https://80.96.122.90:8443/api/v1/namespaces/nubomedia/services";
+    private final static String consoleIpAddress = "https://openshift:8443/api/v1/namespaces/nubomedia/services";
     private final static String authToken = "0lCfgEQQRrPShWFyrSd48lnkAbUYhNwJg_qTalq6E0k";
 
     public static String defineService() throws IOException{
