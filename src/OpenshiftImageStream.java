96a1859a7a7c6ed4d46a40dbc61fdb4b886b2378
Pasquale Carlo Maiorano Picone <charliemaiors@gmail.com>
2016-03-05 10:48:10 +0100

    Update OpenshiftImageStream.java

--- src/OpenshiftImageStream.java
+++ src/OpenshiftImageStream.java
@@ -11,7 +11,7 @@ import java.io.*;
 
 public class OpenshiftImageStream {
 
-    private final static String consoleIpAddress = "https://80.96.122.90:8443/oapi/v1/namespaces/nubomedia/imagestreams";
+    private final static String consoleIpAddress = "https://openshift:8443/oapi/v1/namespaces/nubomedia/imagestreams";
     private final static String authToken = "0lCfgEQQRrPShWFyrSd48lnkAbUYhNwJg_qTalq6E0k";
 
     public static String createStream() throws IOException {
