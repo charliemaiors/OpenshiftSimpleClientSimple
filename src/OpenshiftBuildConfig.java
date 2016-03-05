dfb75d8b633848abeb2e8fe9ceba4e6201a1b37e
Pasquale Carlo Maiorano Picone <charliemaiors@gmail.com>
2016-03-05 10:47:37 +0100

    Update OpenshiftBuildConfig.java

--- src/OpenshiftBuildConfig.java
+++ src/OpenshiftBuildConfig.java
@@ -9,7 +9,7 @@ import java.io.*;
 
 public class OpenshiftBuildConfig {
 
-    private final static String consoleIpAddress = "https://80.96.122.90:8443/oapi/v1/namespaces/nubomedia/buildconfigs";
+    private final static String consoleIpAddress = "https://openshift:8443/oapi/v1/namespaces/nubomedia/buildconfigs";
     private final static String authToken = "0lCfgEQQRrPShWFyrSd48lnkAbUYhNwJg_qTalq6E0k";
     public static String doRequest() throws IOException {
 
